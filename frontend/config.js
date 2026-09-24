6693c97bd7d5af5f95c890c529f5e05464b545b8
gregoryFebvre <gregoryFebvre@users.noreply.github.com>
2026-09-24 19:37:37 +0200

    Update API_URL in frontend config

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // URL de déploiement Apps Script (se termine par /exec)
-window.CONFIG = { API_URL: "https://script.google.com/macros/s/AKfycbwPR2Is767YDmGwr7YwhsgP9SKXCMAjC1b2YOv3ystqhPDVo_wpZnkh9yrPDKlNr0LUqA/exec" };
+window.CONFIG = { API_URL: "https://script.google.com/macros/s/AKfycbw7OtSucohrQB1eUUs4IRMUqd-Vtb4gsWkio7xMrbEuI3qT-yRvDTQHOV_DnmOHccNJJg/exec" };
