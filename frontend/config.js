8f62812aa4c8e1fe2e74a103ca5f75be82ee9c14
gregoryFebvre <gregoryFebvre@users.noreply.github.com>
2026-09-24 11:39:43 +0200

    Update API_URL in config.js to production URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // URL de déploiement Apps Script (se termine par /exec)
-window.CONFIG = { API_URL: "COLLER_ICI_URL_APPS_SCRIPT" };
+window.CONFIG = { API_URL: "https://script.google.com/macros/s/AKfycbwPR2Is767YDmGwr7YwhsgP9SKXCMAjC1b2YOv3ystqhPDVo_wpZnkh9yrPDKlNr0LUqA/exec" };
